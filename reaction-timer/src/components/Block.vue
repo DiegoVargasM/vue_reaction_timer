0aaad4bd252b414b2f4e17760303905f092608ac
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-10 09:53:00 -0500

    Add timer functionality to Block component

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -1,36 +1,48 @@
 <template>
-	<div class="block" v-if="showBlock">
-		Click me!
-	</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click me!</div>
 </template>
 
 <script>
 export default {
-	props: ['delay'],
-	data() {
-		return {
-			showBlock: false,
-		}
-	},
-	mounted() {
-		// args: callback, delay
-		setTimeout(() => { 
-			this.showBlock = true
-			console.log(this.delay)
-		}, this.delay)
-	},
-}
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    // args: callback, delay
+    setTimeout(() => {
+      this.showBlock = true;
+      this.startTimer();
+    }, this.delay);
+  },
+  methods: {
+    // every 10ms add 10ms to reactionTime
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log(this.reactionTime);
+    },
+  },
+};
+s;
 </script>
 
 <style>
 .block {
-	width: 400px;
-	border-radius: 20px;
-	background: rgb(253, 0, 0);
-	color: white;
-	text-align: center;
-	padding: 100px 0;
-	margin: 50px auto;
+  width: 400px;
+  border-radius: 20px;
+  background: rgb(253, 0, 0);
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 50px auto;
 }
-
 </style>
