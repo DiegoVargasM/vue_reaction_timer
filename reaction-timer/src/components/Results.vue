9fbee4b04cdeef8562c71eeaf7a0b03df080e045
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-10 10:36:59 -0500

    Add rank and style restults

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,11 +1,32 @@
 <template>
   <p>Reaction Time: {{ score }} ms</p>
+  <p class="rank">Rank: {{ rank }}</p>
 </template>
 
 <script>
 export default {
   props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Ninja Fingers!";
+    } else if (this.score < 400) {
+      this.rank = "Average Joe.";
+    } else {
+      this.rank = "Snail peaced...";
+    }
+  },
 };
 </script>
 
-<style></style>
+<style>
+.rank {
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: #f00;
+}
+</style>
